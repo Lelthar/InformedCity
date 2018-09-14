62d680be4ff2f18a6d3512d8127d20efb6ddde36
lelthar <geraldm1998@hotmail.com>
2018-09-14 11:07:39 -0600

    Documentos de string modificados  con más datos y las rutas del backend

--- App/InformedCity/app/src/main/java/controlador/DaoApi.java
+++ App/InformedCity/app/src/main/java/controlador/DaoApi.java
@@ -1,9 +1,20 @@
 package controlador;
 
+import org.json.JSONObject;
+
 /**
  * Created by gerald on 13/09/18.
  */
 
 public class DaoApi {
+    private JSONObject informacion;
+    private Conexion conexion;
+
+    public DaoApi() {
+    }
+
+    public void consultaApi() {
+
+    }
 
 }
