15f59543d571f7b49d295c75aee661f7e0595979
lelthar <geraldm1998@hotmail.com>
2018-09-13 22:21:51 -0600

    Controlador con todos los atributos

--- App/InformedCity/app/src/main/java/controlador/Controlador.java
+++ App/InformedCity/app/src/main/java/controlador/Controlador.java
@@ -13,9 +13,43 @@ public class Controlador {
     private Usuario usuario;
     private Evento evento;
     private ArrayList<Evento> listaEventos;
+    private DaoApi daoApi;
+    private GestorEventos gestorEventos;
+    private GestorMapa gestorMapa;
+    private GestorUsuarios gestorUsuarios;
 
     public Controlador() {
        setUsuario(new Usuario());
+       setDaoApi(new DaoApi());
+       setEvento(new Evento());
+       setListaEventos(new ArrayList<Evento>());
+       setGestorEventos(new GestorEventos());
+       setGestorMapa(new GestorMapa());
+       setGestorUsuarios(new GestorUsuarios());
+    }
+
+    public GestorEventos getGestorEventos() {
+        return gestorEventos;
+    }
+
+    public void setGestorEventos(GestorEventos gestorEventos) {
+        this.gestorEventos = gestorEventos;
+    }
+
+    public GestorMapa getGestorMapa() {
+        return gestorMapa;
+    }
+
+    public void setGestorMapa(GestorMapa gestorMapa) {
+        this.gestorMapa = gestorMapa;
+    }
+
+    public GestorUsuarios getGestorUsuarios() {
+        return gestorUsuarios;
+    }
+
+    public void setGestorUsuarios(GestorUsuarios gestorUsuarios) {
+        this.gestorUsuarios = gestorUsuarios;
     }
 
     public Usuario getUsuario() {
@@ -40,5 +74,13 @@ public class Controlador {
 
     public void setListaEventos(ArrayList<Evento> listaEventos) {
         this.listaEventos = listaEventos;
+    }
+
+    public DaoApi getDaoApi() {
+        return daoApi;
+    }
+
+    public void setDaoApi(DaoApi daoApi) {
+        this.daoApi = daoApi;
     }
 }
