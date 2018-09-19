38bbe19e217c804afc8ee0cc4df96f51777618a2
lelthar <geraldm1998@hotmail.com>
2018-09-19 00:08:52 -0600

    Agregar eventos futuros listo

--- App/InformedCity/app/src/main/java/vista/InicioSesion.java
+++ App/InformedCity/app/src/main/java/vista/InicioSesion.java
@@ -79,8 +79,8 @@ public class InicioSesion extends AppCompatActivity {
     }
 
     public void registro() {
-        //Intent intent= new Intent(InicioSesion.this, Registro.class);
-        Intent intent= new Intent(InicioSesion.this, AgregarEventoActual.class);
+        Intent intent= new Intent(InicioSesion.this, Registro.class);
+        //Intent intent= new Intent(InicioSesion.this, AgregarEventoActual.class);
         startActivity(intent);
     }
 
